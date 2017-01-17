9ca89356687d9b832f1a1c6ae69c2bd037029fe4
Chialin Chou <chialin668@gmail.com>
2017-01-16 23:47:13 -0800

    test aws

--- test/test_sample.py
+++ test/test_sample.py
@@ -1,9 +1,26 @@
+import boto3
+
+def dynamodb():
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table('users')
+    response = table.get_item(
+        Key={
+            'username': 'janedoe',
+            'last_name': 'Doe'
+        }
+    )
+    item = response['Item']
+    print(item)
+    return item
+
+
 def func(x):
+    print x
     return x + 1
 
-def test_answer():
+def test_func():
     assert func(3) == 4
 
-def test_ans1():
-    assert func(2) == 3
+def test_aws():
+    assert dynamodb() != None
 
